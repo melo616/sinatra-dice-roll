762bd8ace919a171b8ad7ce5c3fc9a89084b919b
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-08-06 18:19:02 +0000

    added all erb files

--- dice.rb
+++ dice.rb
@@ -27,10 +27,9 @@ get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  #this makes it an instance variable rather than a local variable
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -38,17 +37,15 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
   die_roll = rand(1..20)
 
-  outcome = "You rolled a #{die_roll}."
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  @outcome = "You rolled a #{die_roll}."
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -60,8 +57,7 @@ get("/dice/5/4") do
 
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
 end
